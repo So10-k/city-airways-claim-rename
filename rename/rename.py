dd3fbe1989a3d0b00049d2a179c28ec0ac1684be
Nicklaus <100449899+Nicklaus-s@users.noreply.github.com>
2023-04-09 09:29:54 -0500

    Thread check

--- rename/rename.py
+++ rename/rename.py
@@ -1,16 +1,12 @@
 import discord
 from discord.ext import commands
+from core import checks
 
 class Rename(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_plugins_ready(self, thread):
-        await thread.reply(embed = discord.Embed(
-        description = "Sorry, but it seems like you forgot to add a <request> argument.",
-        color = 0x06c9ff
-    ))
+    @checks.thread_only()
 
     @commands.command()
     async def rename(self, ctx, *, request):
@@ -20,28 +16,33 @@ class Rename(commands.Cog):
                 color = 0x06c9ff
             ))
             await ctx.message.add_reaction('❎')
+            return
 
         try:
             await ctx.channel.edit(name = request)
             await ctx.message.add_reaction('✅')
+            return
         except discord.errors.Forbidden:
             await ctx.reply(embed = discord.Embed(
                 description = "Sorry, but it seems I can't perform this action due to my permission levels.",
                 color = 0x06c9ff
             ))
             await ctx.message.add_reaction('❎')
+            return
         except discord.errors.RateLimited:
             await ctx.reply(embed = discord.Embed(
                 description = 'Sorry, but it seems I have been rate limited.',
                 color = 0x06c9ff
             ))
             await ctx.message.add_reaction('❎')
+            return
         except:
             await ctx.reply(embed = discord.Embed(
                 description = 'An unexpected error occurred.',
                 color = 0x06c9ff
             ))
             await ctx.message.add_reaction('❎')
+            return
 
 async def setup(bot):
     await bot.add_cog(Rename(bot))
