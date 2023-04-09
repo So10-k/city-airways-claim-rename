d1c2e6019d8dbd169b03f4769bbe3431bc6fee98
Nicklaus <100449899+Nicklaus-s@users.noreply.github.com>
2023-04-09 15:01:02 -0500

    Update rename.py

--- rename/rename.py
+++ rename/rename.py
@@ -6,26 +6,33 @@ class Rename(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    client = commands.Bot('$')
+
     @checks.thread_only()
 
-    @commands.Cog.listener()
-    async def on_command_error(ctx):
+    @client.event()
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.reply(embed = discord.Embed(
+                description = 'Sorry, but it seems I have been rate limited.',
+                color = 0x06c9ff
+            ))
+            await ctx.message.add_reaction('❎')
+
+            raise error
+
         await ctx.reply(embed = discord.Embed(
-            description = 'Sorry, but it seems I have been rate limited.',
+            description = 'An unexpected error occurred. It has been logged to the bot console.\n\n```py\n' + error + '```',
             color = 0x06c9ff
         ))
         await ctx.message.add_reaction('❎')
 
+        raise error
+
+    @commands.cooldown(2, 5, commands.BucketType.user)
+
     @commands.command()
     async def rename(self, ctx, *, request):
-        if not request:
-            await ctx.reply(embed = discord.Embed(
-                description = "Sorry, but it seems like you forgot to add a <request> argument.",
-                color = 0x06c9ff
-            ))
-            await ctx.message.add_reaction('❎')
-            return
-
         try:
             await ctx.channel.edit(name = request)
             await ctx.message.add_reaction('✅')
@@ -33,20 +40,6 @@ class Rename(commands.Cog):
         except discord.errors.Forbidden:
             await ctx.reply(embed = discord.Embed(
                 description = "Sorry, but it seems I can't perform this action due to my permission levels.",
-                color = 0x06c9ff
-            ))
-            await ctx.message.add_reaction('❎')
-            return
-        except discord.errors.RateLimited:
-            await ctx.reply(embed = discord.Embed(
-                description = 'Sorry, but it seems I have been rate limited.',
-                color = 0x06c9ff
-            ))
-            await ctx.message.add_reaction('❎')
-            return
-        except:
-            await ctx.reply(embed = discord.Embed(
-                description = 'An unexpected error occurred.',
                 color = 0x06c9ff
             ))
             await ctx.message.add_reaction('❎')
