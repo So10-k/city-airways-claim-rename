efa4637d79e5cc4406a945e3903f825e9a3763b6
Nicklaus <100449899+Nicklaus-s@users.noreply.github.com>
2023-04-10 07:09:45 -0500

    Update rename.py

--- rename/rename.py
+++ rename/rename.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from core import checks
+import datetime
 
 class Rename(commands.Cog):
     def __init__(self, bot):
@@ -13,13 +14,13 @@ class Rename(commands.Cog):
         try:
             embed = discord.Embed(
                 title = 'Changing channel name..',
-                description = ('Updating channel name to %s! Please keep in mind this may take a while, no need to run the command again.' % request),
+                description = ('Updating channel name to %s!' % request),
                 color = discord.Color.yellow()
             )
-            embed.set_footer(text = 'Nicklaus#5688 • Rename')
+            embed.timestamp = datetime.datetime.utcnow()
+            embed.set_footer(text = 'Rename • Nicklaus#56880')
 
             edit = await ctx.reply(embed = embed)
-            await ctx.message.add_reaction('⏱️')
 
             await ctx.channel.edit(name = request) # Edit channel name, finally.
 
@@ -28,10 +29,10 @@ class Rename(commands.Cog):
                 description = ('Updated channel name to %s!' % request),
                 color = discord.Color.green()
             )
-            embed.set_footer(text = 'Nicklaus#5688 • Rename')
+            embed.timestamp = datetime.datetime.utcnow()
+            embed.set_footer(text = 'Rename • Nicklaus#56880')
 
             await edit.edit(embed = embed)
-            await ctx.message.clear_reactions()
             await ctx.message.add_reaction('✅')
         except discord.errors.Forbidden:
             embed = discord.Embed(
@@ -39,7 +40,8 @@ class Rename(commands.Cog):
                 description = "Uh oh, it seems I can't perform this action due to my permission levels.",
                 color = discord.Color.red() # 0x06c9ff
             )
-            embed.set_footer(text = 'Nicklaus#56880 • Rename')
+            embed.timestamp = datetime.datetime.utcnow()
+            embed.set_footer(text = 'Rename • Nicklaus#56880')
 
             await ctx.reply(embed = embed)
             await ctx.message.add_reaction('❎')
