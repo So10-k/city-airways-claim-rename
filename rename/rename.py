aad380de2ebabbae61026b280a9879bac7d550dd
Nicklaus <100449899+Nicklaus-s@users.noreply.github.com>
2023-07-14 15:21:45 -0500

    Add permission group check, add fallback exception

--- rename/rename.py
+++ rename/rename.py
@@ -1,19 +1,27 @@
 import discord
 from discord.ext import commands
+
 from core import checks
+from core.models import PermissionLevel
+
 import datetime
 
 class Rename(commands.Cog):
+    """Rename a thread!"""
+
     def __init__(self, bot):
         self.bot = bot
 
     @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
 
     @commands.command()
     async def rename(self, ctx, *, request):
         try:
             await ctx.message.add_reaction('⏰')
+            
             await ctx.channel.edit(name = request) # Edit channel name.
+            
             await ctx.message.clear_reactions()
             await ctx.message.add_reaction('✅')
         except discord.errors.Forbidden:
@@ -23,9 +31,14 @@ class Rename(commands.Cog):
                 color = discord.Color.red()
             )
             embed.timestamp = datetime.datetime.utcnow()
-            embed.set_footer(text = 'Rename • Nicklaus#5688')
+            embed.set_footer(text = 'Rename')
 
             await ctx.reply(embed = embed)
+            
+            await ctx.message.clear_reactions()
+            await ctx.message.add_reaction('❌')
+        except:
+            await ctx.message.clear_reactions()
             await ctx.message.add_reaction('❌')
 
 async def setup(bot):
